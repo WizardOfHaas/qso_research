6de423ee96001a1d90da682b53e0b4310bfb5e40
Sean Haas <wizardofhaas1024@gmail.com>
2017-02-15 20:06:53 -0800

    added mongodb and conversion to beta to sdss_parse

--- sdss_parse.pl
+++ sdss_parse.pl
@@ -4,35 +4,52 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use MongoDB;
 
-my $MJD = 55475;
-my $FIBER = 210;
-my $PLATEID = 4207;
+my $client = MongoDB->connect('mongodb://localhost');
+my $collection = $client->ns('qso_data.lines');
 
-my $url = "http://dr9.sdss3.org/spectrumDetail?mjd=$MJD&fiber=$FIBER&plateid=$PLATEID";
-print $url."\n";
-my $raw_data = `curl "$url"`;
+open my $cfg_file, "<", $ARGV[0];
 
-my @t = split(/<tbody>|<\/tbody>/, $raw_data);
+while(<$cfg_file>){
+	my ($PLATEID, $MJD, $FIBER) = $_ =~ /spec\-([0-9]+)\-([0-9]+)\-([0-9]+)/;
 
-foreach my $entry (split(/<\/tr>/, $t[-2])){
+	my $url = "http://dr9.sdss3.org/spectrumDetail?mjd=$MJD&fiber=$FIBER&plateid=$PLATEID";
+	#print $url."\n";
+	my $raw_data = `curl "$url"`;
+
+	my @t = split(/<tbody>|<\/tbody>/, $raw_data);
+
+	foreach my $entry (split(/<\/tr>/, $t[-2])){
 	my ($name) = $entry =~ /<\!\-\- Line Name \-\-><td class\="name">(.*)<\/td>/;
 	my ($wavelength) = $entry =~ /<\!\-\- Wavelength \-\-><td>(.*)<\/td>/;
 	my ($z) = $entry =~ /<\!\-\- linez \-\-><td>-(.*)<\/td>/;
-        my ($sigma) = $entry =~ /<\!\-\- linesigma \-\-><td>(.*)<\/td>/;
-        my ($area) = $entry =~ /<\!\-\- linearea \-\-><td>(.*)<\/td>/;
-        my ($contlevel) = $entry =~ /<\!\-\- linecontlevel \-\-><td>(.*)<\/td>/;
+    my ($sigma) = $entry =~ /<\!\-\- linesigma \-\-><td>(.*)<\/td>/;
+    my ($area) = $entry =~ /<\!\-\- linearea \-\-><td>(.*)<\/td>/;
+    my ($contlevel) = $entry =~ /<\!\-\- linecontlevel \-\-><td>(.*)<\/td>/;
 
-	if($name){
-		my $data = {
-			name => $name,
-			wavelength => 0+ $wavelength,
-			z => ($z? 0+ $z : 0),
-			sigma => 0+ $sigma,
-			area => 0+ $area,
-			ccontlevel => 0+ $contlevel
-		};
+		if($name && defined $z){
+			#Convert wavelength to v, for refrence purposes.
+			my $CIV = 1549;
+			my $abs_z = ($wavelength/$CIV) - 1;
+			my $RC = (1 + $z)/(1 + $abs_z);
+			my $betaC = (($RC**2) - 1)/(($RC**2) + 1);
+			my $beta = $betaC * (-300000);
 
-		print Dumper $data;
+			my $data = {
+				name => $name,
+				wavelength => 0+ $wavelength,
+				z => ($z? 0+ $z : 0),
+				sigma => 0+ $sigma,
+				area => 0+ $area,
+				contlevel => 0+ $contlevel,
+				SID => (0+ $PLATEID)."-".(0+ $MJD)."-".(0+ $FIBER),
+				beta => 0+ $beta
+			};
+
+			$collection->insert($data);
+
+			#print Dumper $data;
+		}
 	}
 }
